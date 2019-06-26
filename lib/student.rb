6bd5a0e64a2253ed0aea4266bdc3aa25b1cd746f
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-26 23:55:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,10 +34,10 @@ class Student
    sql = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
-      
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute
     
  end
  
