cf72d864ef08bce1d626a11225e7483659e06bde
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-27 01:02:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,4 +44,13 @@ class Student
   end
  end
  
+ def update
+   sql = <<-SQL
+    UPDATE students
+    SET name = ?, grade = ?
+    WHERE id = ?
+   SQL
+   DB[:conn].execute(sql, self.name, self.grade, self.id)
+ end
+ 
 end
